15eaa3226ed11df621349d60f6edfa641f8ba856
Andrew Walker <andreww@ir113v.niees.group.cam.ac.uk>
2007-08-07 12:00:33 +0100

    Memory managemnet and code for potential_list

--- Fortran/potential_list.f90
+++ Fortran/potential_list.f90
@@ -15,49 +15,80 @@ type two_body_pot
      type(two_body_pot), pointer :: next_pot => null()
  end type two_body_pot
 
- type(two_body_pot), pointer :: first_pot
+ type(two_body_pot), pointer :: root_pot
  type(two_body_pot), pointer :: read_pointer
  type(two_body_pot), pointer :: write_pointer
 
 contains 
 
  subroutine potential_list_init()
-     allocate (first_pot)
-     read_pointer => first_pot
-     write_pointer => write_pointer
+     allocate (root_pot)
+     read_pointer => root_pot
+     write_pointer => root_pot
 
  end subroutine potential_list_init
 
  subroutine  potential_list_exit()
 
-  if (associated first_pot%next_pot) then
+  if (associated(root_pot%next_pot)) then
      ! Remove this pot, point first_pot at next_pot and try again
-     
-     deallocate(first_pot)
-  else
-     ! Just remove this pot and clean up globals 
-  end if 
+     call remove_pots(root_pot%next_pot)
+  endif
+  deallocate(read_pointer)
+  deallocate(write_pointer)
+  deallocate(root_pot)
 
  end subroutine potential_list_exit
+
+ recursive subroutine remove_pots (this_pot)
+
+     type(two_body_pot), pointer :: this_pot
+
+     if (associated(this_pot%next_pot)) then
+         call remove_pots(this_pot%next_pot)
+     endif
+     deallocate(this_pot)
+
+ end subroutine remove_pots
 
 
  subroutine add_potential (atom1, atom2, parameters, parameter_name, potid)
 
      character(len=*), intent(in) :: atom1, atom2
-     real, pointer, dimension(:), intent(in) :: parameters
-     character(len=*), pointer, dimension(:), intent(in) :: parameter_name
+     real, dimension(:), intent(in) :: parameters
+     character(len=*), dimension(:), intent(in) :: parameter_name
      character(len=*), intent(in) :: potid
 
-     
+     type(two_body_pot), pointer :: new_pot
+
+     allocate(new_pot)
+     write_pointer%next_pot => new_pot
+     write_pointer => write_pointer%next_pot
+     write_pointer%atoms(1) = atom1
+     write_pointer%atoms(2) = atom2
+     write_pointer%parameters(:) = parameters(:)
+     write_pointer%parameter_name(:) = parameter_name(:)
+     write_pointer%potid = potid
 
  end subroutine add_potential
 
  subroutine read_next_potential(atom1, atom2, parameters, parameter_name, potid)
 
      character(len=*), intent(out) :: atom1, atom2
-     real, pointer, dimension(:), intent(out) :: parameters
-     character(len=*), pointer, dimension(:), intent(out) :: parameter_name
+     real, dimension(:), intent(out) :: parameters
+     character(len=*), dimension(:), intent(out) :: parameter_name
      character(len=*), intent(out) :: potid
+
+     if (associated(read_pointer%next_pot)) then
+         read_pointer => read_pointer%next_pot
+         atom1 = read_pointer%atoms(1)
+         atom2 = read_pointer%atoms(2)
+         parameters(:) = read_pointer%parameters(:)
+         parameter_name(:) = read_pointer%parameter_name(:)
+         potid = read_pointer%potid
+     else
+         stop "Cannot read from unwritten potential"
+     endif
 
  end subroutine read_next_potential
  
