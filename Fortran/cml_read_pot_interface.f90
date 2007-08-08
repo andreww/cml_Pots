4177aed5de667ca567545b4264ac9403bbe48ce8
Andrew Walker <andreww@ir113v.niees.group.cam.ac.uk>
2007-08-08 09:00:17 +0100

    only in the interface.

--- Fortran/cml_read_pot_interface.f90
+++ Fortran/cml_read_pot_interface.f90
@@ -1,6 +1,12 @@
 module cml_pot
 
-  use cml_pot_sax
-  use cml_pot_data
+! This module just provides and interface needed to allow 
+! sax parsing and recovery of data by the caller without
+! having to use multiple modules. There may be a case 
+! to provide some wrapper functons here
+
+  use cml_pot_sax, only : cml_read_pots
+  use cml_pot_data, only : next_potential, read_potential, number_of_pots, &
+         & potential_list_init, potential_list_exit
 
 end module cml_pot
