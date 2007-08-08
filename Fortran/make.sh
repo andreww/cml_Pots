d5bfcd13e0d39afdb5d7897e0c1fc832cd0210cb
Andrew Walker <andreww@ir113v.niees.group.cam.ac.uk>
2007-08-08 08:38:38 +0100

    Fix order of compilation

--- Fortran/make.sh
+++ Fortran/make.sh
@@ -3,7 +3,7 @@ cd FoX
 make
 cd ..
 g95  -Wall -g  -Wunused-vars -Wuninitialized -fbounds-check -ftrace=full -c two_body_pot_str.f90
-g95  -Wall -g  -Wunused-vars -Wuninitialized -fbounds-check -ftrace=full -c cml_read_pot.f90 `./FoX/FoX-config --fcflags --sax`
 g95  -Wall -g  -Wunused-vars -Wuninitialized -fbounds-check -ftrace=full -c potential_list.f90
+g95  -Wall -g  -Wunused-vars -Wuninitialized -fbounds-check -ftrace=full -c cml_read_pot.f90 `./FoX/FoX-config --fcflags --sax`
 g95  -Wall -g  -Wunused-vars -Wuninitialized -fbounds-check -ftrace=full -c readpot.f03
 g95  readpot.o cml_read_pot.o potential_list.o `./FoX/FoX-config --libs --sax` -g -o readpot
